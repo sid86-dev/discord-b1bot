e15304e2355caa6f83b2e9b026e4915f47224be5
sid86-dev <sid86harth@gmail.com>
2021-08-01 21:44:06 +0530

    babebot

---
+++ main.py
@@ -0,0 +1,37 @@
+import discord
+import os
+import random 
+import pytz 
+
+from keep_alive import keep_alive
+from prsaw import RandomStuffV2
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print("Bot is connected. Logged in as {0.user}".format(client))
+@client.event
+async def on_message(message):
+  msg = message.content
+
+  rs = RandomStuffV2() 
+
+  lst = rs.get_ai_response(msg)
+
+  response = lst['message']
+
+
+
+  if message.author == client.user:
+    return
+  else:
+    await message.channel.send(response)
+
+  rs.close()
+
+
+my_secret = 'TOKEN'
+
+keep_alive()
+client.run(my_secret)
